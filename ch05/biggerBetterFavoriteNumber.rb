67d333e4ca22e03bd896be51bab013c8a45f05ee
Kangdi Yu <gus@deleon.org.mx>
2017-12-22 20:10:09 +0800

    Chapter 5 assignment added

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'Hi, could you tell me your favorite number?'
+num = gets.chomp.to_i
+num = num + 1
+puts 'em... How about ' + num.to_s + '? I think that is better.'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, and what\'s your first name?'
+nFirst  = gets.chomp
+puts 'Your middle name? (If you don\'t have a middle name, directly press ENTER.)'
+nMiddle = gets.chomp
+puts 'And your last name?'
+nLast = gets.chomp
+puts 'Your name is ' + nFirst + ' ' + nMiddle + ' ' + nLast + '? What a lovely name!'
+puts 'Pleased to meet you, ' + nFirst + ' ' + nMiddle + ' ' + nLast + '!'
